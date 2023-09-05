0de89bd37d1089711daa980f01a7d6eea87dbc99
ThumburuAditya <chandramouliadityat@gmail.com>
2023-09-06 01:18:40 +0530

    Adding the code

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "aws_docdb_cluster" "main" {
   storage_encrypted = true
   kms_key_id = var.kms_arn
   port = var.port_no
-  vpc_security_group_ids = [aws_security_group.sg.id]
+  vpc_security_group_ids = [aws_security_group.main.id]
 }
 
 resource "aws_docdb_cluster_parameter_group" "main" {
